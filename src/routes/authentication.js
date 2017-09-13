a77775eb74b9855197518d0a692efcb6011bd6c0
maighdlyn <emmamaighdlyn@gmail.com>
2017-09-13 14:36:42 -0700

    Add sign-up ejs & route

--- src/routes/authentication.js
+++ src/routes/authentication.js
@@ -1,11 +1,11 @@
 const router = require('express').Router()
 
-router.get('/', (req, res) => {
-  res.render('login')
-})
-
 router.get('/login', (req, res) => {
   res.render('authentication/login')
+})
+
+router.get('/sign-up', (req, res) => {
+  res.render('authentication/sign-up')
 })
 
 module.exports = router
