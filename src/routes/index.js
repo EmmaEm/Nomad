e736b4bab3a34852404637808232dc813b4c462f
maighdlyn <emmamaighdlyn@gmail.com>
2017-09-13 16:31:44 -0700

    Add profile page ejs & route that renders it

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,11 @@
 const router = require('express').Router()
 const authentication = require('./authentication')
 const home = require('./home')
+const profile = require('./profile')
 
+// NOTE: There's probably a more efficient way to 'use' all the files in a folder. Figure it out later.
 router.use('/', authentication)
 router.use('/', home)
+router.use('/', profile)
 
 module.exports = router
