ece26ec5c68c0e9a8a7463445b3da8c8d09b1ef6
maighdlyn <emmamaighdlyn@gmail.com>
2017-09-13 15:16:49 -0700

    Add home page & reviews partial

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router()
 const authentication = require('./authentication')
+const home = require('./home')
 
 router.use('/', authentication)
+router.use('/', home)
 
 module.exports = router
