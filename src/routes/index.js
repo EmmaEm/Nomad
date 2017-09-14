4e93c9c547323329b4175020327f8a14426086b2
maighdlyn <emmamaighdlyn@gmail.com>
2017-09-13 17:39:00 -0700

    Add city ejs & route rendering city

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,10 +2,13 @@ const router = require('express').Router()
 const authentication = require('./authentication')
 const home = require('./home')
 const profile = require('./profile')
+const city = require('./city')
+
 
 // NOTE: There's probably a more efficient way to 'use' all the files in a folder. Figure it out later.
 router.use('/', authentication)
 router.use('/', home)
 router.use('/', profile)
+router.use('/', city)
 
 module.exports = router
